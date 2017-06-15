97fd0cb065337977a1def1f4085e21e86223f3f7
Garrett Beatty <gbt@Garretts-MacBook-Pro.local>
2017-06-15 19:32:14 -0400

    changed to keras

---
+++ train.py
@@ -0,0 +1,111 @@
+import os
+import cv2
+import numpy as np
+from keras.utils import np_utils
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Convolution2D, MaxPooling2D
+from sklearn.model_selection import train_test_split
+
+
+letter_map = {
+    'a': 0,
+    'b': 1,
+    'c': 2,
+    'd': 3,
+    'e': 4,
+    'f': 5,
+    'g': 6,
+    'h': 7,
+    'i': 8,
+    'j': 9,
+    'k': 10,
+    'l': 11,
+    'm': 12,
+    'n': 13,
+    'o': 14,
+    'p': 15,
+    'q': 16,
+    'r': 17,
+    's': 18,
+    't': 19,
+    'u': 20,
+    'v': 21,
+    'w': 22,
+    'x': 23,
+    'y': 24,
+    'z': 25,
+    '0': 26,
+    '1': 27,
+    '2': 28,
+    '3': 29,
+    '4': 30,
+    '5': 31,
+    '6': 32,
+    '7': 33,
+    '8': 34,
+    '9': 35,
+}
+
+n_letters = len(letter_map)
+
+def build_dataset(dataset_path):
+    """
+
+    This function will generate the file paths and corresponding labels for this dataset.
+    :param dataset_path: Dataset path.
+    :type dataset_path: str
+    :return: files and labels
+    :rtype: list, list
+    """
+    x, y = [], []
+    for root, subFolders, files in os.walk(dataset_path):
+        for file in files:
+            x.append(os.path.join(root, file))
+            label = letter_map[os.path.basename(os.path.normpath(root))]
+            y.append(label)
+
+    return x, y
+
+files, y = build_dataset('dataset')
+
+x = []
+for i, file in enumerate(files):
+    print(i, 'out of', len(files))
+    image = cv2.imread(file)
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    x.append(image)
+
+x = np.array(x)
+y = np.array(y)
+
+width, height = 400, 400
+
+x = np.reshape(x, (-1, width, height, 1))
+
+x = x.astype('float32')
+x /= 255
+
+y = np_utils.to_categorical(y, n_letters)
+
+x_train, x_test, y_train, y_test = train_test_split(x, y)
+
+model = Sequential()
+
+model.add(Convolution2D(32, 3, 3, activation='relu', input_shape=(width, height, 1)))
+model.add(Convolution2D(32, 3, 3, activation='relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.25))
+
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(n_letters, activation='softmax'))
+
+model.compile(loss='categorical_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train,
+          batch_size=32, epochs=10, verbose=1)
+score = model.evaluate(x_test, y_test, verbose=0)
