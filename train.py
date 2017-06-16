5647a48f9fefb8657d19d0208b75669cf26527e6
Garrett Beatty <gbt@Garretts-MacBook-Pro.local>
2017-06-16 09:55:34 -0400

    updated arch

--- train.py
+++ train.py
@@ -5,6 +5,7 @@ from keras.utils import np_utils
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Convolution2D, MaxPooling2D
+from keras.constraints import maxnorm
 from sklearn.model_selection import train_test_split
 
 
@@ -92,14 +93,24 @@ x_train, x_test, y_train, y_test = train_test_split(x, y)
 
 model = Sequential()
 
-model.add(Convolution2D(32, 3, 3, activation='relu', input_shape=(width, height, 1)))
-model.add(Convolution2D(32, 3, 3, activation='relu'))
-model.add(MaxPooling2D(pool_size=(2,2)))
-model.add(Dropout(0.25))
-
+model.add(Convolution2D(32, (3, 3), input_shape=(width, height, 1), activation='relu', padding='same'))
+model.add(Dropout(0.2))
+model.add(Convolution2D(32, (3, 3), activation='relu', padding='same'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Convolution2D(64, (3, 3), activation='relu', padding='same'))
+model.add(Dropout(0.2))
+model.add(Convolution2D(64, (3, 3), activation='relu', padding='same'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Convolution2D(128, (3, 3), activation='relu', padding='same'))
+model.add(Dropout(0.2))
+model.add(Convolution2D(128, (3, 3), activation='relu', padding='same'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Flatten())
-model.add(Dense(128, activation='relu'))
-model.add(Dropout(0.5))
+model.add(Dropout(0.2))
+model.add(Dense(1024, activation='relu', kernel_constraint=maxnorm(3)))
+model.add(Dropout(0.2))
+model.add(Dense(512, activation='relu', kernel_constraint=maxnorm(3)))
+model.add(Dropout(0.2))
 model.add(Dense(n_letters, activation='softmax'))
 
 model.compile(loss='categorical_crossentropy',
--- webcam.py
+++ webcam.py
@@ -1,9 +1,14 @@
 import cv2
+from train import width, height
 
 cap = cv2.VideoCapture(0)
 while True:
     ret, frame = cap.read()
     #resizes to correct input
+
+    resized = cv2.resize(frame, (width, height))
+    gray = cv2.cvtColor(resized, cv2.COLOR_RGB2GRAY)
+
     cv2.imshow('frame', frame)
     k = cv2.waitKey(33)
     if k == 27:  # Esc key to stop
