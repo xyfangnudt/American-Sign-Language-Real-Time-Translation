1f3a219c588fcdbb8cde19ed59d0e7d80e5dd674
Garrett Beatty <gbt@Garretts-MacBook-Pro.local>
2017-06-26 21:17:45 -0400

    updated program

--- webcam.py
+++ webcam.py
@@ -1,41 +1,57 @@
 import cv2
 import numpy as np
 
-# Constants for finding range of skin color in YCrCb
-min_YCrCb = np.array([0, 133, 77],np.uint8)
-max_YCrCb = np.array([255, 173, 127], np.uint8)
-
-roi_rec_cords = ((100, 150), (500, 600))
+roi_rec_cords = ((100, 100), (500, 600))
 
 cap = cv2.VideoCapture(0)
+fgbg = cv2.createBackgroundSubtractorMOG2(0, 7)
+
+bg_captured = False
+skin = None
+
+
+def capture_background(frame):
+    fgmask = fgbg.apply(frame)
+    res = cv2.bitwise_and(frame, frame, mask=fgmask)
+    return res
+
+
 while True:
     ret, frame = cap.read()
     #resizes to correct input
+    frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+    frame = cv2.GaussianBlur(frame, (21, 21), 0)
 
     x1, x2 = roi_rec_cords[0][0], roi_rec_cords[1][0]
     y1, y2 = roi_rec_cords[0][1], roi_rec_cords[1][1]
-    roi_rec = frame[y1:y2, x1:x2] #numpy switches x and y..
-    # cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 255, 255))
+    roi_rec = frame[y1:y2, x1:x2]  # numpy switches x and y..
 
-    #Convert image to YCrCb
-    imageYCrCb = cv2.cvtColor(roi_rec, cv2.COLOR_BGR2YCR_CB)
+    cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 255, 255))
 
-    # Find region with skin tone in YCrCb image
-    skinRegion = cv2.inRange(imageYCrCb, min_YCrCb, max_YCrCb)
+    if bg_captured:
 
-    # Do contour detection on skin region
-    im2, contours, hierarchy = cv2.findContours(skinRegion, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        skin = capture_background(roi_rec)
+        # blur_skin = cv2.GaussianBlur(skin, (41, 41), 0)
+        _, thresh = cv2.threshold(skin, 10, 255, cv2.THRESH_BINARY)
 
-    # Draw the contour on the source image
-    for i, c in enumerate(contours):
-        area = cv2.contourArea(c)
-        if area > 1000:
-            cv2.drawContours(roi_rec, contours, i, (0, 255, 0), 3)
+        cv2.imshow('skin', skin)
 
+
+
+    # cv2.imshow('skin', skin)
     cv2.imshow('frame', frame)
+
     k = cv2.waitKey(33)
     if k == 27:  # Esc key to stop
         break
+    elif k == -1:
+        pass
+    elif k == 99:
+        bg_captured = True
+    else:
+        print(k)
+
+
 
 # When everything done, release the capture
 cap.release()
